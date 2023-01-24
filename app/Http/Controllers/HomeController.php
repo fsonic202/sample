9dc5792fc4d8f8eb915ac64c8801df8d83ee1ed7
fsonic202 <f.oseh@aol.com>
2023-01-24 18:13:44 -0500

    update create post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,8 +11,15 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = Category::all();
+        //$allCategories = Category::all();
+        $categories = Category::all();
+        $posts = Post::when(request('category_id'), function($query) {
+            $query->where('category_id', request('category_id'));
+        })
+        ->latest()
+        ->get();
 
-        return view( 'index', ['categories' => $allCategories]);
+        return view('index', 
+            compact('categories', 'posts'));
     }
 }
